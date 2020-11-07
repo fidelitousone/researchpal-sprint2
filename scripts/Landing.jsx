c0297208790faf7d54fafbf45aab1300cd937404
Robert Argasinski <ra536@njit.edu>
2020-11-07 00:33:37 +0000

    react bootstrap is set up, although styling is not showing up

--- scripts/Landing.jsx
+++ scripts/Landing.jsx
@@ -1,9 +1,13 @@
-import * as React from 'react'
+import * as React from 'react';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Landing() {
     return (
         <div className="Landing">
             Hello World!
+            <br />
+            <Button variant="outline-primary">Login</Button>{' '}
         </div>
     )
 }
