8853f1933f66dee0a19f6712b4513eea16023e98
Robert Argasinski <ra536@njit.edu>
2020-11-11 01:43:45 +0000

    Clean up landing page

--- scripts/Landing.jsx
+++ scripts/Landing.jsx
@@ -7,16 +7,22 @@ import 'bootstrap/dist/css/bootstrap.css';
 export default function Landing() {
   return (
     <div className="Landing">
-      Hello World!
-      <br />
-      <br />
-      <GoogleAuth />
-      <br />
-      <br />
-      <MicrosoftAuth />
-      <br />
-      <br />
-      <FacebookAuth />
+      <div className="jumbotron">
+        <h1>ResearchPal</h1>
+        <p>The simple research organization tool, designed by researchers for researchers</p>
+      </div>
+      <h2 align="center">Log in below:</h2>
+      <div className="d-flex justify-content-center">
+          <div className="align-self-center px-2">
+            <GoogleAuth />
+          </div>
+          <div className="align-self-center px-2">
+            <MicrosoftAuth />
+          </div>
+          <div className="align-self-center px-2">
+            <FacebookAuth />
+          </div>
+      </div>
     </div>
   );
 }
