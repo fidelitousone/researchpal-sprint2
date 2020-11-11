76181f259f91633ad999593cc1f0bfceee773f74
Jerry <js843@njit.edu>
2020-11-10 19:40:36 -0500

    lint the entire front end

--- scripts/FacebookButton.jsx
+++ scripts/FacebookButton.jsx
@@ -1,26 +1,26 @@
 import * as React from 'react';
-import { Socket } from './Socket';
 import FacebookLogin from 'react-facebook-login';
+import Socket from './Socket';
 
-function handleSubmit(response) {
+export default function FacebookAuth() {
+  function handleSubmit(response) {
     Socket.emit('new_facebook_user', {
-        'response': response,
+      response,
     });
-    
     console.log('Sent new facebook user to server!');
-}
+  }
 
-function responseFacebookSuccess(response){
-  console.log("Success:", response);
-  handleSubmit(response);
-}
- 
-export function FacebookAuth() {
-    return (
-        <FacebookLogin
-        appId="257908695617151"
-        autoLoad={false}
-        fields="name,email,picture"
-        callback={responseFacebookSuccess} />
-    );
+  function responseFacebookSuccess(response) {
+    console.log('Success:', response);
+    handleSubmit(response);
+  }
+
+  return (
+    <FacebookLogin
+      appId="257908695617151"
+      autoLoad={false}
+      fields="name,email,picture"
+      callback={responseFacebookSuccess}
+    />
+  );
 }
--- scripts/Landing.jsx
+++ scripts/Landing.jsx
@@ -1,21 +1,22 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Button } from 'react-bootstrap';
-import { GoogleAuth } from './GoogleButton';
-import { MicrosoftAuth } from './MicrosoftButton';
-import { FacebookAuth } from './FacebookButton';
+import GoogleAuth from './GoogleButton';
+import FacebookAuth from './FacebookButton';
+import MicrosoftAuth from './MicrosoftButton';
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Landing() {
-    return (
-        <div className="Landing">
-            Hello World!
-            <br /><br />
-            <GoogleAuth />
-            <br /><br />
-            <MicrosoftAuth />
-            <br /><br />
-            <FacebookAuth />
-        </div>
-    )
+  return (
+    <div className="Landing">
+      Hello World!
+      <br />
+      <br />
+      <GoogleAuth />
+      <br />
+      <br />
+      <MicrosoftAuth />
+      <br />
+      <br />
+      <FacebookAuth />
+    </div>
+  );
 }
