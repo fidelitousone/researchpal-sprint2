b5396a8e14febb932e7071c89b871b72e16b1163
nootify <48371116+nootify@users.noreply.github.com>
2020-11-12 13:01:46 -0500

    Separate commonly used fixtures to conftest.py

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,18 +1,7 @@
-import uuid
-
 import pytest
-
-from app import socketio, Users, AuthType
 
 
 # pylint: disable = redefined-outer-name
-@pytest.fixture()
-def mocked_uuid(mocker):
-    mock_uuid = mocker.patch.object(uuid, "uuid4", autospec=True)
-    mock_uuid.return_value = uuid.UUID(hex="aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa")
-    return mock_uuid
-
-
 @pytest.fixture()
 def mocked_google_response():
     mocked_response = {
@@ -67,48 +56,6 @@ def mocked_invalid_response():
 def mocked_login_request():
     mocked_request = {"email": "fake@e.mail"}
     return mocked_request
-
-
-@pytest.fixture()
-def mocked_user_model(mocked_uuid):
-    mocked_uuid = mocked_uuid()
-    return Users(
-        "fake@e.mail",
-        str(mocked_uuid),
-        "A Name",
-        AuthType.GOOGLE,
-        "link",
-    )
-
-
-@pytest.fixture(scope="module")
-def app():
-    from app import app  # pylint: disable = import-outside-toplevel
-
-    yield app
-
-
-@pytest.fixture()
-def client(app):
-    app.config["TESTING"] = True
-
-    with app.test_client() as test_client:
-        yield test_client
-
-
-@pytest.fixture()
-def socketio_client(app):
-    yield socketio.test_client(app)
-
-
-@pytest.fixture()
-def db(app):
-    from app import db  # pylint: disable = import-outside-toplevel
-
-    with app.app_context():
-        db.create_all()
-        yield db
-        db.drop_all()
 
 
 # pylint: disable = no-self-use
