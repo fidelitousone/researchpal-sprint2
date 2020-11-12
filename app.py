5da12b5f03f44f03e295a2df39b531205eab6fb9
markgalesi <markgalesi170@gmail.com>
2020-11-12 23:55:25 +0000

    projects update on dashboard load

--- app.py
+++ app.py
@@ -138,18 +138,22 @@ def on_request_user_data():
 
 @socketio.on("create_project")
 def on_new_project(data):
-    email = session.get('user')
-    with app.app_context():
-        user_info = db.session.query(Users).filter(Users.email == email).one().json()
-    owner_id=user_info['user_id']
-    project_id = uuid.uuid4()
-    project_name = data["project_name"]
-    sources = []
-    with app.app_context():
-        new_project = Projects(project_id, owner_id, project_name, sources)
-        db.session.add(new_project)
-        db.session.commit()
-    emit_projects(email,owner_id)
+    if not session.get('user') is None:
+        email = session.get('user')
+        with app.app_context():
+            user_info = db.session.query(Users).filter(Users.email == email).one().json()
+        owner_id=user_info['user_id']
+        project_id = uuid.uuid4()
+        project_name = data["project_name"]
+        sources = []
+        with app.app_context():
+            new_project = Projects(project_id, owner_id, project_name, sources)
+            db.session.add(new_project)
+            db.session.commit()
+        emit_projects(email,owner_id)
+    else:
+        print("not logged in")
+        
 
 
 @app.route("/")
