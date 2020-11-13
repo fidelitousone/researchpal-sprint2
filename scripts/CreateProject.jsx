68d823a0f0b0d5636ec1884bf3591010688be297
Robert Argasinski <ra536@njit.edu>
2020-11-13 19:12:25 +0000

    Add testing for empty string or whitespace, along with warning notification

--- scripts/CreateProject.jsx
+++ scripts/CreateProject.jsx
@@ -1,14 +1,25 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import Socket from './Socket';
 import { Button } from 'react-bootstrap';
+import { Alert } from 'react-bootstrap';
 
 function handleSubmit(event) {
     var projectName = document.getElementById("name_input");
-    Socket.emit('create_project', {
+    console.log("PROJECT: ");
+    console.log(projectName.value);
+    
+    if(/\s/g.test(projectName.value) || projectName.value === ''){
+        ReactDOM.render(<Alert className="alert-warning">Warning: Project name was empty or only whitespace.  Please try again with a valid project name.</Alert>, document.getElementById("notif_project"));
+    } else {
+        ReactDOM.render(<span></span>, document.getElementById("notif_project"))
+        Socket.emit('create_project', {
         'project_name': projectName.value,
         'socketid' : Socket.id,
-    });
-    console.log('Sent the project ' + projectName.value + ' to server!');
+        });
+        console.log('Sent the project ' + projectName.value + ' to server!');
+    }
+    
     projectName.value = '';
     
     event.preventDefault();
@@ -18,6 +29,7 @@ export function CreateButton() {
     return (
         <div align="center">
             <form onSubmit={handleSubmit}>
+                <div id="notif_project"></div>
                 <input id="name_input" placeholder="Enter new project name"></input>
                 <Button type="submit" className="btn-primary">Create!</Button>
             </form>
