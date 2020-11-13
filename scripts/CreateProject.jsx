d3ded157a3390276515f4d0b98813fe9b6ff088c
Jerry <js843@njit.edu>
2020-11-13 11:26:53 -0500

    refactor

--- scripts/CreateProject.jsx
+++ scripts/CreateProject.jsx
@@ -1,26 +1,26 @@
 import * as React from 'react';
-import Socket from './Socket';
 import { Button } from 'react-bootstrap';
+import Socket from './Socket';
 
 function handleSubmit(event) {
-    var projectName = document.getElementById("name_input");
-    Socket.emit('create_project', {
-        'project_name': projectName.value,
-        'socketid' : Socket.id,
-    });
-    console.log('Sent the project ' + projectName.value + ' to server!');
-    projectName.value = '';
-    
-    event.preventDefault();
+  const projectName = document.getElementById('name_input');
+  Socket.emit('create_project', {
+    project_name: projectName.value,
+    socketid: Socket.id,
+  });
+  console.log(`Sent the project ${projectName.value} to server!`);
+  projectName.value = '';
+
+  event.preventDefault();
 }
 
 export function CreateButton() {
-    return (
-        <div align="center">
-            <form onSubmit={handleSubmit}>
-                <input id="name_input" placeholder="Enter new project name"></input>
-                <Button type="submit" className="btn-primary">Create!</Button>
-            </form>
-        </div>
-    );
+  return (
+    <div align="center">
+      <form onSubmit={handleSubmit}>
+        <input id="name_input" placeholder="Enter new project name" />
+        <Button type="submit" className="btn-primary">Create!</Button>
+      </form>
+    </div>
+  );
 }
--- scripts/Project.jsx
+++ scripts/Project.jsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
-import { Button, ButtonGroup, Badge, Image, DropdownButton } from 'react-bootstrap';
+import {
+  Button, ButtonGroup, Badge, Image, DropdownButton,
+} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -7,30 +9,34 @@ export default function Project() {
   return (
     <div className="Project">
       <div display="flex" flex="0 0 auto" align="center">
-       
-        <span className="h1" position="absolute" left="0">Project <Badge className="badge-primary">Name</Badge></span>
-        
-        
-        <DropdownButton id="dropdown-basic-button" title={
-          <Image src="static/profile-blank.jpg" className="rounded-circle border" width="50px" height="50px"/>
-        }>
-        </DropdownButton>
-       
+
+        <span className="h1" position="absolute" left="0">
+          Project
+          <Badge className="badge-primary">Name</Badge>
+        </span>
+
+        <DropdownButton
+          id="dropdown-basic-button"
+          title={
+            <Image src="static/profile-blank.jpg" className="rounded-circle border" width="50px" height="50px" />
+        }
+        />
+
       </div>
       <br />
       <div className="d-flex justify-content-center">
         <ButtonGroup aria-label="Basic example">
-            <Link to="/">
+          <Link to="/">
             <Button className="btn-outline-primary">Landing</Button>
-            </Link>
-            <Link to="/home">
+          </Link>
+          <Link to="/home">
             <Button className="btn-outline-primary">Dashboard</Button>
-            </Link>
-            <Link to="/project">
+          </Link>
+          <Link to="/project">
             <Button className="btn-outline-primary">Project</Button>
-            </Link>
-          </ButtonGroup>
-        </div>
+          </Link>
+        </ButtonGroup>
+      </div>
     </div>
   );
 }
