405726e05ef739eaeaae95274af38dfea8be7ee2
Robert Argasinski <ra536@njit.edu>
2020-12-02 09:23:46 -0500

    Login page working with buttons

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import {
   Col, Container, Row, Image,
 } from 'react-bootstrap';
-import GoogleAuth from './GoogleButton';
-import FacebookAuth from './FacebookButton';
-import MicrosoftAuth from './MicrosoftButton';
 import 'bootstrap/dist/css/bootstrap.css';
 import NavigationBar from './NavigationBar';
 
@@ -27,24 +23,6 @@ export default function Landing() {
         </Container>
       </Jumbotron>
 
-      <h2 style={{ textAlign: 'center' }}>
-        Log in below:
-      </h2>
-      <Container>
-        <Row noGutters="true" style={{ justifyContent: 'center', alignItems: 'center' }}>
-          <Col lg="2">
-            <GoogleAuth />
-          </Col>
-          <Col lg="2">
-            <MicrosoftAuth />
-          </Col>
-          <Col lg="2">
-            <Link to="/home">
-              <FacebookAuth />
-            </Link>
-          </Col>
-        </Row>
-      </Container>
       <NavigationBar />
     </div>
   );
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,32 @@
 import * as React from 'react';
+import { Container, Row, Col, } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import GoogleAuth from './GoogleButton';
+import FacebookAuth from './FacebookButton';
+import MicrosoftAuth from './MicrosoftButton';
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Login() {
-  return (<p>LOGIN</p>);
+  return (
+    <>
+      <h2 style={{ textAlign: 'center' }}>
+        Log in below:
+      </h2>
+      <Container>
+        <Row noGutters="true" style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <Col lg="2">
+            <GoogleAuth />
+          </Col>
+          <Col lg="2">
+            <MicrosoftAuth />
+          </Col>
+          <Col lg="2">
+            <Link to="/home">
+              <FacebookAuth />
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
