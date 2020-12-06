d2e8a8742af4b7ddd123776f8003303e3538da38
nootify <48371116+nootify@users.noreply.github.com>
2020-12-06 13:09:10 -0500

    Fix incorrectly nested <a> HTML

--- src/MainNavBar.jsx
+++ src/MainNavBar.jsx
@@ -6,35 +6,27 @@ import 'bootstrap/dist/css/bootstrap.css';
 export default function MainNavBar() {
   return (
     <Navbar bg="dark" variant="dark">
-      <Link to="/">
-        <Navbar.Brand href="/">
-          <img
-            src="ResearchPal.svg"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-            alt=""
-          />
-          {' '}
-          ResearchPal
-        </Navbar.Brand>
-      </Link>
+      <Navbar.Brand as={Link} to="/">
+        <img
+          src="ResearchPal.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt=""
+        />
+        {' '}
+        ResearchPal
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Link to="/about">
-            <Nav.Link href="/about">About Us</Nav.Link>
-          </Link>
-          <Link to="/pricing">
-            <Nav.Link href="/pricing">Pricing</Nav.Link>
-          </Link>
-          <Link to="/future">
-            <Nav.Link href="/future">Our Future</Nav.Link>
-          </Link>
+          <Nav.Link as={Link} to="/about">About Us</Nav.Link>
+          <Nav.Link as={Link} to="/pricing">Pricing</Nav.Link>
+          <Nav.Link as={Link} to="/future">Our Future</Nav.Link>
         </Nav>
-        <Link to="/contact">
+        <Nav.Link as={Link} to="/contact">
           <Button variant="primary">Contact Us</Button>
-        </Link>
+        </Nav.Link>
       </Navbar.Collapse>
     </Navbar>
   );
