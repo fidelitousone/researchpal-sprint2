12272cf2d96b600e14e1ef87afc831ed4f76f525
Robert Argasinski <ra536@njit.edu>
2020-12-08 18:16:02 -0500

    Create card template for About Us page, yet to be completed content-wise

--- src/About.jsx
+++ src/About.jsx
@@ -1,4 +1,8 @@
 import * as React from 'react';
+import {
+  Container, Row, Col, Card,
+} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import MainNavBar from './MainNavBar';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -6,7 +10,80 @@ export default function About() {
   return (
     <>
       <MainNavBar />
-      <p>TODO: About Page</p>
+      <br />
+      <div align="center">
+        <h1>About Us</h1>
+      </div>
+      <br />
+      <Container>
+        <Row xs={1}>
+          <Col>
+            <Card className="text-center">
+              <Card.Header>Who are we?</Card.Header>
+              <Card.Body>
+                We are a project group for CS 490-001, a software-engineering course at NJIT.
+                <br />
+                Our names are Robert Argasinski, Eugene Cha, Mark Galesi, and Jatinder Singh.
+                <br />
+                If you would like to reach out to us, click
+                {' '}
+                <Link to="/contact">
+                  here
+                </Link>
+                .
+              </Card.Body>
+            </Card>
+            <br />
+            <Card className="text-center">
+              <Card.Header>What did we build?</Card.Header>
+              <Card.Body>
+                We are a project group for CS 490-001, a software-engineering course at NJIT.
+                <br />
+                Our names are Robert Argasinski, Eugene Cha, Mark Galesi, and Jatinder Singh.
+                <br />
+                If you would like to reach out to us, click
+                {' '}
+                <Link to="/contact">
+                  here
+                </Link>
+                .
+              </Card.Body>
+            </Card>
+            <br />
+            <Card className="text-center">
+              <Card.Header>How did we build it?</Card.Header>
+              <Card.Body>
+                We are a project group for CS 490-001, a software-engineering course at NJIT.
+                <br />
+                Our names are Robert Argasinski, Eugene Cha, Mark Galesi, and Jatinder Singh.
+                <br />
+                If you would like to reach out to us, click
+                {' '}
+                <Link to="/contact">
+                  here
+                </Link>
+                .
+              </Card.Body>
+            </Card>
+            <br />
+            <Card className="text-center">
+              <Card.Header>How can I access it?</Card.Header>
+              <Card.Body>
+                We are a project group for CS 490-001, a software-engineering course at NJIT.
+                <br />
+                Our names are Robert Argasinski, Eugene Cha, Mark Galesi, and Jatinder Singh.
+                <br />
+                If you would like to reach out to us, click
+                {' '}
+                <Link to="/contact">
+                  here
+                </Link>
+                .
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
