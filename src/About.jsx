b0164905cdd190425c67d6277f7b8b5d91545c7f
Robert Argasinski <ra536@njit.edu>
2020-12-08 21:07:07 -0500

    Fill in information for the About Us page

--- src/About.jsx
+++ src/About.jsx
@@ -21,7 +21,7 @@ export default function About() {
             <Card className="text-center">
               <Card.Header>Who are we?</Card.Header>
               <Card.Body>
-                We are a project group for CS 490-001, a software-engineering course at NJIT.
+                We are a project group for CS 490-001, a software engineering course at NJIT.
                 <br />
                 Our names are Robert Argasinski, Eugene Cha, Mark Galesi, and Jatinder Singh.
                 <br />
@@ -37,7 +37,44 @@ export default function About() {
             <Card className="text-center">
               <Card.Header>What did we build?</Card.Header>
               <Card.Body>
-                We are a project group for CS 490-001, a software-engineering course at NJIT.
+                For our project, we chose to build ResearchPal,
+                a tool designed to help students and researchers
+                organize their sources and generate citations.
+              </Card.Body>
+            </Card>
+            <br />
+            <Card className="text-center">
+              <Card.Header>How does it work?</Card.Header>
+              <Card.Body>
+                Once on our homepage, users can log in with either their Google,
+                Facebook, or Microsoft accounts.
+                <br />
+                On successful login, users will be presented with a list of their
+                available projects.
+                <hr />
+                From there, users can choose an existing project or create a new one.
+                <br />
+                After clicking into the desired project, users can input individual source URLs
+                or upload a file with URLs separated by newlines.
+                <br />
+                A list of sources can also be downloaded to assist with migrating sources between
+                project or sharing data between applications.
+                <hr />
+                Finally, the user can access a bibliography page and choose the corresponding format
+                for citations (MLA and APA are currently supported).
+                <br />
+                Just like with sources, a list of citations can be downloaded from the
+                bibliography page.
+                <br />
+                The user can choose which citations should be included in the
+                exported bibliography and which should be ignored.
+              </Card.Body>
+            </Card>
+            <br />
+            <Card className="text-center">
+              <Card.Header>How did we build it?</Card.Header>
+              <Card.Body>
+                We are a project group for CS 490-001, a software engineering course at NJIT.
                 <br />
                 Our names are Robert Argasinski, Eugene Cha, Mark Galesi, and Jatinder Singh.
                 <br />
@@ -51,15 +88,17 @@ export default function About() {
             </Card>
             <br />
             <Card className="text-center">
-              <Card.Header>How did we build it?</Card.Header>
+              <Card.Header>Why does it matter?</Card.Header>
               <Card.Body>
-                We are a project group for CS 490-001, a software-engineering course at NJIT.
+                As students, we know the difficulty of staying organized when
+                conducting research.
                 <br />
-                Our names are Robert Argasinski, Eugene Cha, Mark Galesi, and Jatinder Singh.
+                Because of this, we chose to create a simple, intuitive, FREE tool
+                to help manage the research process.
                 <br />
-                If you would like to reach out to us, click
+                To learn more about what sets us apart click
                 {' '}
-                <Link to="/contact">
+                <Link to="/pricing">
                   here
                 </Link>
                 .
@@ -69,18 +108,13 @@ export default function About() {
             <Card className="text-center">
               <Card.Header>How can I access it?</Card.Header>
               <Card.Body>
-                We are a project group for CS 490-001, a software-engineering course at NJIT.
-                <br />
-                Our names are Robert Argasinski, Eugene Cha, Mark Galesi, and Jatinder Singh.
-                <br />
-                If you would like to reach out to us, click
+                Our live deployment is available at
                 {' '}
-                <Link to="/contact">
-                  here
-                </Link>
+                <a href="https://rocky-sea-29898.herokuapp.com/">https://rocky-sea-29898.herokuapp.com/</a>
                 .
               </Card.Body>
             </Card>
+            <br />
           </Col>
         </Row>
       </Container>
